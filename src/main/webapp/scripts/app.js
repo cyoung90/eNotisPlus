daef8434521bc72d7147473df01673a32d6d191b
cyoung90 <chlckdud78@gmail.com>
2017-11-08 00:13:41 +0900

    새벽 열심

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -9,7 +9,7 @@
 //		cardTemplate: document.querySelector('.cardTemplate'),
 //		container: document.querySelector('.main'),
 //		addDialog: document.querySelector('.dialog-container'),
-		
+		autoLogin: false,
 		user: {"id":"32089829","mail":"chayoung_choi@daekyo.co.kr"},
 		daysOfWeek: ['월', '화', '수', '목', '금', '토', '일']
 	};
@@ -20,22 +20,43 @@
  * Event listeners for UI elements
  * 
  ******************************************************************************/
+	$(document).ready(function(){
+		if ( app.autoLogin && app.user.ID ){
+			app.moveComponent("/components/home/home.jsp");
+		}
+	});
 	
 	document.getElementById('btn_login').addEventListener('click', function() {
-		app.loginProcess();
+		//app.loginProcess();
 		
-	    var select = document.getElementById('selectCityToAdd');
-	    var selected = select.options[select.selectedIndex];
-	    var key = selected.value;
-	    var label = selected.textContent;
-	    if (!app.selectedCities) {
-	      app.selectedCities = [];
-	    }
-	    app.getForecast(key, label);
-	    app.selectedCities.push({key: key, label: label});
-	    app.saveSelectedCities();
-	    app.toggleAddDialog(false);
+		// test 사번
+		if ( $("#id").val() == 'e' ) {
+			$("#id").val("8300120");
+			$("#pw").val("init1234");
+		}
+		
+		var id = document.getElementById('id').value;
+		var pw = document.getElementById('pw').value;
+		
+		
+		app.loginProcess(id, pw);
+		
+//	    var select = document.getElementById('selectCityToAdd');
+//	    var selected = select.options[select.selectedIndex];
+//	    var key = selected.value;
+//	    var label = selected.textContent;
+//	    if (!app.selectedCities) {
+//	      app.selectedCities = [];
+//	    }
+//	    app.getForecast(key, label);
+//	    app.selectedCities.push({key: key, label: label});
+//	    app.saveSelectedCities();
+//	    app.toggleAddDialog(false);
 	    
+		console.log( app.user.ID );
+		if ( app.user.ID ){
+			alert("로그인");
+		}
 	});
 	
 	
@@ -50,13 +71,85 @@
  * Methods to update/refresh the UI
  * 
  ******************************************************************************/
-
+	app.saveUserInfo = function(data) {
+		
+		var lastUpdateDt = new Date();
+		data.lastUpdateDt = lastUpdateDt;
+		
+		var user = JSON.stringify(data);
+	    localStorage.user = user;	// 로컬에 저장
+	    localStorage.autoLogin = $("#chk_autoLogin").val();
+	}
 
 /*******************************************************************************
  * 
  * Methods for dealing with the model
  * 
  ******************************************************************************/
+	// 로그인
+	app.loginProcess = function(id, pw) {
+		
+		var host = window.location.host;
+		
+		var url = 'http://' + host + '/main/loginProcess.do?id=' + id + '&pw=' + pw;
+		
+		if ( 'caches' in window ) {
+			caches.match(url).then(function(response) {
+				if (response) {
+					response.json().then(function updateFromCache(json) {
+						var results = json;
+						var msg = results.msg;
+						var resultCd = results.resultCd;
+						
+						if (resultCd == "200") {
+							var userInfo = results.userInfo;
+							app.saveUserInfo(userInfo);
+							app.moveComponent("/components/home/home.jsp");
+						}
+						
+						console.log("caches");
+					});
+				}
+			});
+		}
+		// Fetch the latest data.
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200) {
+					var response = JSON.parse(request.response);
+					
+					var results = response;
+					var msg = results.msg;
+					var resultCd = results.resultCd;
+					
+					if (resultCd == "200") {
+						var userInfo = results.userInfo;
+						app.saveUserInfo(userInfo);
+					} else {
+						alert(msg);
+					}
+					
+					console.log("XMLHttpRequest");
+				}
+			} else {
+				// Return the initial weather forecast since no data is available.
+				//app.updateForecastCard(initialWeatherForecast);
+			}
+		};
+//		var comSubmit = new ComSubmit();
+//		comSubmit.addParam("id", $("#id").val());
+//		comSubmit.addParam("pw", $("#pw").val());
+//		
+//		var formData = $("#commonForm").serialize();
+//		console.log(formData);
+		
+		request.open('GET', url);
+		request.send();
+//		request.open('POST', 'http://' + host + '/main/loginProcess.do');
+//		request.send(formData);
+	};
+	
 	app.updateForecasts = function() {
 		var keys = Object.keys(app.visibleCards);
 			keys.forEach(function(key) {
@@ -64,8 +157,19 @@
 		});
 	};
 
+	var initUser = {
+			id: '8300120',
+			name: '최차영',
+			mail: 'chayoung_choi@daekyo.co.kr',
+			lastUpdateDt: '20171107180000'
+	}
 
-
+	// 화면 이동
+	app.moveComponent = function(url){
+		var comSubmit = new ComSubmit();
+		comSubmit.setUrl(url);
+		comSubmit.submit();
+	}
 /*******************************************************************************
  * 
  * Code required to start the app
@@ -77,11 +181,9 @@
  * (https://gist.github.com/inexorabletash/c8069c042b734519680c)
  ******************************************************************************/
 	app.user = localStorage.user;
+	app.autoLogin = localStorage.autoLogin;
 	if (app.user) {
 	    app.user = JSON.parse(app.user);
-	    app.user.forEach(function(city) {
-	    	app.getForecast(city.key, city.label);
-	    });
 	} else {
 		
 	}
